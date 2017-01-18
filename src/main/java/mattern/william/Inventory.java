eed80f3c23af98ac924c4b0914c664a13973be92
William Mattern <williammattern@zipcoders-MacBook-Pro-3.local>
2017-01-18 18:30:17 -0500

    building Inventory

---
+++ src/main/java/mattern/william/Inventory.java
@@ -0,0 +1,21 @@
+package mattern.william;
+
+import java.util.ArrayList;
+
+/**
+ * Created by williammattern on 1/18/17.
+ */
+public class Inventory {
+    //tracks products
+    ArrayList products = new ArrayList(1);
+    double totalValueOfAllProducts;
+
+    public double getTotalValueOfAllProducts() {
+        for (int i = 0; i < products.size(); i++){
+
+        }
+
+
+        return totalValueOfAllProducts;
+    }
+}
---
+++ src/main/java/mattern/william/Product.java
@@ -0,0 +1,7 @@
+package mattern.william;
+
+/**
+ * Created by williammattern on 1/18/17.
+ */
+public class Product {
+}
