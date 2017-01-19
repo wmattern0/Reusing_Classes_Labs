23387ef9fea78b9fb2c1dacb816217ef29a2b821
William Mattern <williammattern@zipcoders-MacBook-Pro-3.local>
2017-01-18 20:10:10 -0500

    ArrayList rotator updated

---
+++ src/main/java/mattern/william/MyList.java
@@ -0,0 +1,60 @@
+package mattern.william;
+
+import java.util.ArrayList;
+
+/**
+ * Created by williammattern on 1/18/17.
+ * Created in collaboration with Gerrod Mozeik.  There is a method that rotates the array by one.  This
+ * method is called k times.
+ * To rotate a 6 element array by 2 times, there are 12 swaps that need to be done.
+ */
+
+public class MyList extends ArrayList {
+    ArrayList<Integer> list = new ArrayList<Integer>();
+
+    public static void main(String[] args) {
+        MyList myList = new MyList();
+        int[] arr1 = {1, 2, 3, 4, 5, 6};
+        myList.fillList(arr1);
+        myList.printList();
+        myList.rotateLeft(arr1, 2, arr1.length);
+        myList.fillList(arr1);
+        myList.printList();
+    }
+
+    public void fillList(int[] array) {
+        for (Integer num : array) {
+            list.add(num);
+        }
+    }
+
+    public void printList(){
+        for (Integer num: list){
+            System.out.print(num + " ");
+        }
+        System.out.println("");
+
+    }
+
+    public void rotateLeft(int[] arr1, int rotateBy, int arrayLength) {
+        for (int i = 0; i < rotateBy; i++) {
+            rotateLeftByOne(arr1, arrayLength);
+        }
+    }
+
+    public void rotateLeftByOne(int[] arr1, int arrayLength) {
+        int j, swap;
+        swap = arr1[0];
+        for (j = 0; j < arrayLength - 1; j++) {
+            arr1[j] = arr1[j + 1];
+        }
+        arr1[j] = swap;
+    }
+
+    public void printArray(int[] arr1, int arrayLength) {
+        int h;
+        for (h = 0; h < arrayLength; h++) {
+            System.out.print(arr1[h] + " ");
+        }
+    }
+}
